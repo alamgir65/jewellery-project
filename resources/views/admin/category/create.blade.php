bafeeef6553d685b4f9b6ec9b17411e9303b412e
unknown <alamgirhossaindrubo@gmail.com>
2025-01-09 20:49:48 +0600

    add Category Form added

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -5,5 +5,47 @@
 @endsection
 
 @section('body')
-    This is Add Category Page.
+    <!-- Basic Layout -->
+    <div class="col-xxl">
+        <div class="card mb-6">
+            <div class="card-header d-flex align-items-center justify-content-between">
+                <h5 class="mb-0">Add Category Form</h5> <small class="text-muted float-end">Default label</small>
+            </div>
+            <div class="card-body">
+                <form method="post" action="" enctype="multipart/form-data">
+                    @csrf
+                    <div class="row mb-4">
+                        <label class="col-sm-2 col-form-label">Category Name</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control"name="name" placeholder="Category Name" />
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <label class="col-sm-2 col-form-label">Publish Status</label>
+                        <div class="col-sm-10 pt-3">
+                            <label><input type="radio" name="status" value="1">Published</label>
+                            <label class="mx-4"><input type="radio" name="status" value="0">Unpublished</label>
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <label class="col-sm-2 col-form-label" for="basic-default-email">Category Image</label>
+                        <div class="col-sm-10">
+                            <input type="file" class="form-control" name="image">
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <label class="col-sm-2 col-form-label">Description</label>
+                        <div class="col-sm-10">
+                            <textarea name="description" class="form-control" placeholder="Category Description"></textarea>
+                        </div>
+                    </div>
+                    <div class="row justify-content-end">
+                        <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Create Category</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -5,5 +5,20 @@
 @endsection
 
 @section('body')
-    This is manage Category Page.
+    <!-- DataTable with Buttons -->
+    <div class="card">
+        <div class="card-datatable table-responsive pt-0">
+            <table class="datatables-basic table table-bordered">
+                <thead>
+                <tr>
+                    <th>Sl</th>
+                    <th>Name</th>
+                    <th>Status</th>
+                    <th>Image</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
 @endsection
